71f61745d497369fae2fcffee1b4b5a13a370522
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-10-10 13:52:46 -0500

    Feat: Added ContentProvider backbone

--- app/src/main/java/com/maheshgaya/android/popularmovies/data/MovieProvider.java
+++ app/src/main/java/com/maheshgaya/android/popularmovies/data/MovieProvider.java
@@ -1,7 +1,56 @@
 package com.maheshgaya.android.popularmovies.data;
 
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+
 /**
  * Created by Mahesh Gaya on 10/6/16.
  */
-public class MovieProvider {
+public class MovieProvider extends ContentProvider{
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    static UriMatcher buildUriMatcher(){
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = MovieContract.CONTENT_AUTHORITY;
+        
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        return false;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(Uri uri, String[] strings, String s, String[] strings1, String s1) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public String getType(Uri uri) {
+        return null;
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(Uri uri, ContentValues contentValues) {
+        return null;
+    }
+
+    @Override
+    public int delete(Uri uri, String s, String[] strings) {
+        return 0;
+    }
+
+    @Override
+    public int update(Uri uri, ContentValues contentValues, String s, String[] strings) {
+        return 0;
+    }
 }
