636d06079035a8c6683641f0a3411664e64f717a
Mahesh Gaya <mmaheshgaya@gmail.com>
2016-10-17 19:11:22 -0500

    Feat: Refactor MovieAdapter

--- app/src/main/java/com/maheshgaya/android/popularmovies/MovieAdapter.java
+++ app/src/main/java/com/maheshgaya/android/popularmovies/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.maheshgaya.android.popularmovies;
 
 import android.content.Context;
+import android.database.Cursor;
 import android.graphics.Point;
 import android.util.Log;
 import android.view.Display;
@@ -9,6 +10,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ArrayAdapter;
+import android.widget.CursorAdapter;
 import android.widget.ImageView;
 
 import com.squareup.picasso.Picasso;
@@ -25,67 +27,54 @@ import java.util.Arrays;
  * MovieAdapter
  * Deals with setting images/poster/thumbnail to the gridview
  */
-public class MovieAdapter extends ArrayAdapter<Movie> {
+public class MovieAdapter extends CursorAdapter {
     private static final String LOG_TAG = MovieAdapter.class.getSimpleName();
 
     /**
      * Constructor
      * @param context
-     * @param movies
-     * Change movies array to List
+     * @param cursor
      */
-    public MovieAdapter(Context context, Movie[] movies){
-        super(context, 0, Arrays.asList(movies));
-        //Log.d(LOG_TAG, "MovieAdapter: " + context.getPackageName() + " : " + movies.length);
+    public MovieAdapter(Context context, Cursor cursor, int flags){
+        super(context, cursor, flags);
+    }
+    
+
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        View view =  LayoutInflater.from(context).inflate(R.layout.grid_item_movie, parent, false);
+        ViewHolder viewHolder = new ViewHolder(view);
+        view.setTag(viewHolder);
+        return view;
     }
 
-
-    /**
-     * getView
-     * @param position
-     * @param convertView
-     * @param parent
-     * @return converView
-     * Deals with setting the images for the gridview
-     */
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        //Log.d(LOG_TAG, "getView: method executed");
-        ViewHolder holder;
+    public void bindView(View view, Context context, Cursor cursor) {
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
 
-        Movie movie = getItem(position); //get current movie
-
-        //initialize the thumbnail image view
-        if (convertView == null) {
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.grid_item_movie, parent, false);
-            holder = new ViewHolder();
-            holder.thumbnailImageView = (ImageView)convertView.findViewById(R.id.thumbnail_image_view);
-            convertView.setTag(holder);
-        } else{
-            holder= (ViewHolder) convertView.getTag();
-        }
-
-        //use picasso to deal with getting the images and setting them to the gridview
+        String imageUrl = ""; //cursor.getString(); //TODO: To replace with projection
 
         try {
             Picasso
-                    .with(getContext())
-                    .load(movie.getThumbnailURL())
+                    .with(context)
+                    .load(imageUrl)
                     .error(R.drawable.ic_photo_placeholder)
-                    .into(holder.thumbnailImageView);
+                    .into(viewHolder.thumbnailImageView);
             //Log.d(LOG_TAG, "getView: Picasso executed");
         }catch (java.lang.IllegalArgumentException e){
             Log.e(LOG_TAG, "getView: " + e.getMessage(), e );
             e.printStackTrace();
         }
-
-        return convertView;
     }
 
     /**
      * template for getting views about each gridview item
      */
-    private static class ViewHolder {
+    public static class ViewHolder {
         ImageView thumbnailImageView;
+
+        public ViewHolder(View view){
+            this.thumbnailImageView = (ImageView)view.findViewById(R.id.thumbnail_image_view);
+        }
     }
 }
